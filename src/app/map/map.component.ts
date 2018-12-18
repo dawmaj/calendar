9c529788d46109c4aea7003701d7ce24b6aec2e6
Dawid Majsnerowski <dawid.majsnerowski@wp.pl>
2018-12-18 08:51:01 +0100

    Fix

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 import OlMap from 'ol/Map';
-import OlXYZ from 'ol/source/XYZ';
-import OlTileLayer from 'ol/layer/Tile';
+import OlVectorSource from 'ol/source/Vector';
+import OlVectorLayer from 'ol/layer/Vector';
 import OlView from 'ol/View';
-
+import OlFeature from 'ol/Feature';
+import OlPoint from 'ol/geom/Point';
+import OlXyzSource from 'ol/source/XYZ';
+import OlTileLayer from 'ol/layer/Tile';
+import {Icon, Style} from 'ol/style';
 import { fromLonLat } from 'ol/proj';
+import { markParentViewsForCheck } from '@angular/core/src/view/util';
 
 declare var ol: any;
 
@@ -20,31 +25,52 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   map: OlMap;
-  source: OlXYZ;
-  layer: OlTileLayer;
-  view: OlView;
+    vectorSource: OlVectorSource;
+    vectorLayer: OlVectorLayer;
+    xyzSource: OlXyzSource;
+    tileLayer: OlTileLayer;
+    view: OlView;
+    marker: OlFeature;
   latitude: number = 52.4082663;
   longitude: number = 16.9335199;
 
   ngOnInit() {
 
-    this.source = new OlXYZ({
-      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
-    });
-
-    this.layer = new OlTileLayer({
-      source: this.source
-    });
+    this.marker = new OlFeature({
+      // Added fromLonLat
+      geometry: new OlPoint(fromLonLat([16.9335199, 52.4082663])),
+  });
 
     this.view = new OlView({
       center: fromLonLat([16.9335199, 52.4082663]),
       zoom: 8
     });
 
-    this.map = new OlMap({
+    this.vectorSource = new OlVectorSource({
+      features: [this.marker]
+  });
+
+  this.vectorLayer = new OlVectorLayer({
+      source: this.vectorSource
+  });
+
+  /* XYZ */
+
+  this.xyzSource = new OlXyzSource({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+  });
+
+  this.tileLayer = new OlTileLayer({
+      source: this.xyzSource
+  });
+
+  /* View and map */
+
+  this.map = new OlMap({
       target: 'map',
-      layers: [this.layer],
+      // Added both layers
+      layers: [this.tileLayer, this.vectorLayer],
       view: this.view
-    });
+  });
   }
 }
